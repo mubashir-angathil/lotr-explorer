877785bd5f038fae2d9e74e8fde36a127c8711ae
Mubashir Angathil <mubashirangathil5142@gmail.com>
2024-02-10 22:46:19 +0530

    UI enhancement

---
+++ src/components/data-grid/Style.js
@@ -0,0 +1,17 @@
+import customColors from '../../utils/helpers/constants/colors'
+
+export const dataGridStyle = {
+  dataGrid: {
+    mt: 2,
+    zIndex: 0,
+    boxShadow: 'none',
+    height: 'calc(100vh - 400px)',
+    '& .MuiDataGrid-columnHeader': {
+      backgroundColor: customColors.textPrimary,
+      color: '#ffff'
+    },
+    '& .MuiDataGrid-columnSeparator': {
+      color: '#ffff'
+    }
+  }
+}
